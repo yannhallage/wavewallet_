aeaa02562684353e146ffeb5b960787825482f25
yannhallage <contact.devhllg@gmail.com>
2025-06-02 05:22:34 +0000

    update 05:22

--- src/myComponents/SignUp.jsx
+++ src/myComponents/SignUp.jsx
@@ -1,23 +1,46 @@
 import { motion } from "framer-motion"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { useState } from "react"
-
+import { useState, useRef } from "react"
+import { RotatingLines } from 'react-loader-spinner'
 
 const SignUp = () => {
     const [tester, setTester] = useState(false)
+    // const buttonIndice = useRef('')
+    const [textForButton, setTexForButton] = useState('Suivant')
+
+    const ButtonDurationWithSpinner = () => {
+        setTester(!tester)
+        console.log(tester)
+
+        setTexForButton(
+            <RotatingLiness
+            />
+        )
+        DurationTester();
+    }
+
+    const DurationTester = () => {
+        if (!tester) {
+             setTimeout(() => {
+                setTexForButton('Suivant')
+                setTester(!tester)
+                console.log(tester)
+            }, "2000")
+        }
+    }
     return (
+
         <div className="min-h-screen flex items-center justify-center bg-[#f5f5f5] px-4">
+
             <motion.div
                 className="bg-white rounded-xl shadow-md p-8 w-full max-w-sm text-center"
                 initial={{ opacity: 0, y: 30 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.6 }}
             >
-                {/* Logo */}
                 <h1 className="text-2xl font-semibold mb-6 text-black">WaveWallet</h1>
 
-                {/* Champ email */}
                 <Input
                     placeholder="Email ou numéro de mobile"
                     className="mb-2"
@@ -26,8 +49,9 @@ const SignUp = () => {
                     tester ? (
                         <>
                             <Input
-                                placeholder="Email ou numéro de mobile"
+                                placeholder="Votre mot de passe"
                                 className="mb-2"
+                                type="password"
                             />
                         </>
                     ) : console.log('faux')
@@ -36,33 +60,28 @@ const SignUp = () => {
                     Adresse email oubliée ?
                 </div>
 
-                {/* Bouton Suivant */}
                 <Button className="bg-[#0070ba] w-full rounded-full hover:bg-[#005c9c] mb-4"
                     onClick={
-                        () => {
-                            setTester(!tester)
-                            console.log(tester)
-                        }
+                        ButtonDurationWithSpinner
                     }
                 >
-                    Suivant
+                    {textForButton}
                 </Button>
 
-                {/* Séparateur */}
                 <div className="flex items-center my-4">
                     <div className="flex-1 h-px bg-gray-300" />
                     <span className="px-2 text-gray-500 text-sm">ou</span>
                     <div className="flex-1 h-px bg-gray-300" />
                 </div>
 
-                {/* Bouton secondaire */}
+
                 <Button variant="outline" className="w-full rounded-full"
 
                 >
                     Ouvrir un compte
                 </Button>
 
-                {/* Langue */}
+
                 <div className="mt-6 text-sm text-gray-500 flex items-center justify-center gap-1">
                     <span role="img" aria-label="flag">🇨🇮</span>
                     <select className="bg-transparent focus:outline-none">
@@ -76,3 +95,22 @@ const SignUp = () => {
 }
 
 export default SignUp
+
+
+
+// spinner et autre wey
+const RotatingLiness = () => {
+    return (
+        <RotatingLines
+            visible={true}
+            height="96"
+            width="96"
+            color="grey"
+            strokeWidth="5"
+            animationDuration="0.75"
+            ariaLabel="rotating-lines-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+        />
+    )
+}
